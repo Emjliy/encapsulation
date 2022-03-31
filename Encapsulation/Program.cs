031a20da44ab1a3c23878a8351a8a428d186d033
Emjliy <6p304s8@code.edu.az>
2022-03-31 10:43:10 +0400

    added

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -7,8 +7,18 @@ namespace Encapsulation
         static void Main(string[] args)
         {
             //Student student1 = new Student(15, "Emily" ,"Aghayeva","IT");
-            Group group = new Group();
-            group.AddStudents(student1);
+            //Group group = new Group();
+            //group.AddStudents(student1);
+            int[] arr1 = { 1, 2, 3 };
+            //Console.WriteLine(numberss.Rank);
+            int[] arr = new int[3];
+            arr1.CopyTo(arr, 0);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(arr==arr1);
+            
         }
     }
 }
